30153ac09b5ba138984f9ac3ec3b7addb017ddaa
AnaMolly <analia.mohamdally@gmail.com>
2021-06-27 22:36:19 -0400

    Added a post and get route for post model

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,16 @@
 const router = require('express').Router();
+const { Post, User } = require('../models');
 
+router.get('/', async (req, res) => {
+    try {
+      const postData = await Post.findAll({
+        include: [{ model: User}],
+      });
+      res.status(200).json(postData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
 
 module.exports = router;
